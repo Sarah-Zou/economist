69074573ee535aa901dc41f03c3fb94749f1dd41
Sarah-Zou <sarah.zou.1118@gmail.com>
2025-05-29 16:31:48 -0400

    Add Google Analytics 4 with Measurement ID G-BX0JPBNQ5K to site layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter, Playfair_Display } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
 const playfair = Playfair_Display({ subsets: ['latin'], variable: '--font-playfair' })
@@ -24,6 +25,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${inter.variable} ${playfair.variable}`}>
+      <Head>
+        {/* Google Analytics 4 */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-BX0JPBNQ5K"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-BX0JPBNQ5K');
+          `,
+        }} />
+      </Head>
       <body className="font-sans">
         <Navbar />
         <main className="min-h-screen">
