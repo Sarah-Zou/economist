4fd66466a05ab4a72018bf9f13a0ef4021d30775
Sarah-Zou <sarah.zou.1118@gmail.com>
2025-06-16 14:12:56 -0400

    Fix: Remove incorrect basePath and assetPrefix settings

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/Personal_Website_6th',
-  assetPrefix: '/Personal_Website_6th/',
   images: {
     remotePatterns: [
       {
